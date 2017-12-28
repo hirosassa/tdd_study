6b138eeb6443d845c4b972f96ec4b815deb2a8da
hirosassa <hiro.sassa@gmail.com>
2017-12-28 20:36:45 +0900

    loc 1435

---
+++ chapter11/money/money.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from abc import ABCMeta, abstractmethod
+
+
+class Money(metaclass=ABCMeta):
+    def __init__(self, amount, currency):
+        self._amount = amount
+        self._currency = currency 
+
+    def __str__(self):
+        return str(self._amount) + " " + self._currency
+
+    def __eq__(self, other):
+        return (self._amount == other._amount) and \
+            (self._currency == other._currency)
+
+    def times(self, multiplier):
+        return Money(self._amount * multiplier, self._currency)
+
+    def currency(self):
+        return self._currency
+
+    @staticmethod
+    def dollar(amount):
+        return Money(amount, "USD")
+
+    @staticmethod
+    def franc(amount):
+        return Money(amount, "CHF")
+    
