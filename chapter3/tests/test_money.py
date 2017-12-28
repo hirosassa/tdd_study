faac42f4705983261a50343db0afc24bd1673c70
hirosassa <hiro.sassa@gmail.com>
2017-12-28 13:30:21 +0900

    loc 467

---
+++ chapter3/tests/test_money.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from money import dollar
+
+class MoneyTest(unittest.TestCase):
+    def test_multiplication(self):
+        five = dollar.Dollar(5)
+        product = five.times(2)
+        self.assertEqual(10, product)
+        product = five.times(3)
+        self.assertEqual(15, product)
+
+    def test_equality(self):
+        self.assertTrue(dollar.Dollar(5) == dollar.Dollar(5))
