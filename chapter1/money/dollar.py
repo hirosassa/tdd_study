8509dea7f17a3c296c1c7cda9b38cb91edfd363c
hirosassa <hiro.sassa@gmail.com>
2017-12-28 12:10:59 +0900

    loc 355

---
+++ chapter1/money/dollar.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Dollar:
+    def __init__(self, amount):
+        self.amount = amount
+
+    def times(self, multiplier):
+        self.amount *= multiplier
+    
+
+
+
+
