2cab872f659d634376cf96ea5447366f3d79a1a1
hirosassa <hiro.sassa@gmail.com>
2017-12-31 10:33:30 +0900

    loc 1683

---
+++ chapter13/money/bank.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .money import Money
+from .sum import Sum
+
+class Bank:
+    def reduce(self, source, to):
+        return source.reduce(to)
+
+
+
+
+
+
+
+
+
+
---
+++ chapter13/money/expression.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from abc import ABCMeta, abstractclassmethod
+
+class Expression(metaclass=ABCMeta):
+    pass
