a1dee1c690c1f95ebd499ed32002b6af982437d2
hirosassa <hiro.sassa@gmail.com>
2017-12-31 15:14:54 +0900

    loc 2183

---
+++ chapter16/money/money.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .expression import Expression
+
+
+class Money(Expression):
+    def __init__(self, amount, currency):
+        self._amount = amount
+        self._currency = currency 
+
+    def __str__(self):
+        return str(self._amount) + " " + self._currency
+
+    def __eq__(self, other):
+        return (self._amount == other._amount) and \
+            (self._currency == other._currency)
+
+    def times(self, multiplier):
+        return Money(self._amount * multiplier, self._currency)
+
+    def amount(self):
+        return self._amount
+
+    def currency(self):
+        return self._currency
+
+    def plus(self, addend):
+        from .sum import Sum
+        return Sum(self, addend)
+
+    def reduce(self, bank, to):
+        rate = bank.rate(self.currency(), to)
+        return Money(self.amount() / rate, to)
+
+    @staticmethod
+    def dollar(amount):
+        return Money(amount, "USD")
+
+    @staticmethod
+    def franc(amount):
+        return Money(amount, "CHF")
+    
+
