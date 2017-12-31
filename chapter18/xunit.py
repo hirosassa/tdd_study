57dcee6fb992d40425a4e5f03c476e0c821c9c45
hirosassa <hiro.sassa@gmail.com>
2017-12-31 19:16:33 +0900

    loc 2573

--- chapter18/xunit.py
+++ chapter18/xunit.py
@@ -18,9 +18,13 @@ class WasRun(TestCase):
     def testMethod(self):
         self.wasRun = True
 
-if __name__ == "__main__":
-    test = WasRun("testMethod")
-    print(test.wasRun)
-    test.run()
-    print(test.wasRun)
 
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+    
+if __name__ == "__main__":
+    TestCaseTest("testRunning").run()
