5eb6a758d34d3b47143e14a8c568322a68fc3bf4
hirosassa <hiro.sassa@gmail.com>
2017-12-31 19:18:02 +0900

    loc 2605

--- chapter18/xunit.py
+++ chapter18/xunit.py
@@ -13,7 +13,7 @@ class TestCase:
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
-        super().__init__(name)        
+        super().__init__(name) 
         
     def testMethod(self):
         self.wasRun = True
