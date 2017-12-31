bce4cc4afd8a92ec3d3356f5332bc84b2c527002
hirosassa <hiro.sassa@gmail.com>
2017-12-31 19:09:08 +0900

    loc 2491

---
+++ chapter18/xunit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class WasRun:
+    def __init__(self, name):
+        self.wasRun = None
+
+    def testMethod(self):
+        self.wasRun = True
+
+test = WasRun("testMethod")
+print(test.wasRun)
+test.testMethod()
+print(test.wasRun)
+
+
+
+
+
