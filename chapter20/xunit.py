58999064af94ed942336e682a23f72af8f826231
hirosassa <hiro.sassa@gmail.com>
2018-01-01 13:18:27 +0900

    loc 2764

--- chapter20/xunit.py
+++ chapter20/xunit.py
@@ -8,10 +8,14 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
         
 class WasRun(TestCase):
@@ -21,12 +25,14 @@ class WasRun(TestCase):
     def testMethod(self):
         self.log = self.log + "testMethod "
 
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert("setUp testMethod " == test.log)
+        assert("setUp testMethod tearDown " == test.log)
     
 if __name__ == "__main__":
     TestCaseTest("testTemplateMethod").run()
