1c38ed87c1a7967aba0ba16b604cda3f38b703e1
hirosassa <hiro.sassa@gmail.com>
2018-01-01 13:07:06 +0900

    loc 2697

--- chapter19/xunit.py
+++ chapter19/xunit.py
@@ -15,25 +15,25 @@ class TestCase:
 
         
 class WasRun(TestCase):
-    def testMethod(self):
-        self.wasRun = True
-
     def setUp(self):
         self.wasRun = None
         self.wasSetUp = True
-        
+
+    def testMethod(self):
+        self.wasRun = True        
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+        
     def testRunning(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
 
     def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert(test.wasSetUp)
+        self.test.run()
+        assert(self.test.wasSetUp)
     
 if __name__ == "__main__":
     TestCaseTest("testRunning").run()
