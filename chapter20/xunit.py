49de987373df57ede1d7c6ccb40e44898ed0f1f0
hirosassa <hiro.sassa@gmail.com>
2018-01-01 13:15:50 +0900

    loc 2742

---
+++ chapter20/xunit.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def setUp(self):
+        pass
+
+    def run(self):
+        self.setUp()
+        method = getattr(self, self.name)
+        method()
+
+        
+class WasRun(TestCase):
+    def setUp(self):
+        self.log = "setUp "
+
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+
+
+class TestCaseTest(TestCase):
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod " == test.log)
+    
+if __name__ == "__main__":
+    TestCaseTest("testTemplateMethod").run()
